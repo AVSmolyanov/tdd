ca1967638d89eac1b1aea6e76b78a4eb4b5f5587
AVSmolyanov <ASmolyanov@mail.ru>
2022-06-16 17:12:23 +1000

    Methods realises

--- src/main/java/com/company/BadDataExeption.java
+++ src/main/java/com/company/BadDataExeption.java
@@ -2,5 +2,8 @@ package com.company;
 
 public class BadDataExeption extends Exception {
 
+    public BadDataExeption(String message) {
+        super(message);
+    }
 
 }
--- src/main/java/com/company/Credit.java
+++ src/main/java/com/company/Credit.java
@@ -7,16 +7,22 @@ public class Credit {
 
 
     public Credit(Double amount, Byte period, Double rate) throws BadDataExeption {
+
+        if (rate < 0) throw new BadDataExeption("Ставка по кредиту не может быть отрицательной");
+
+        this.amount = amount;
+        this.period = period;
+        this.rate = rate;
     }
 
 
-    public Double monthPayment()  {
-        return  null;
+    public Double monthPayment() {
+        return amount / period + amount * rate / 100;
     }
 
 
     public Double fullAmount() {
-        return null;
+        return amount + amount * period * rate / 100;
     }
 
 }
